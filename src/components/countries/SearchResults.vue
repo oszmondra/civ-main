3a3576f1ea8f13e0f36a8494e5ba57c044a4703f
oszmondra <oszmodra@gmail.com>
2023-02-28 10:34:16 -0600

    cambio de resultados y diseño

--- src/components/countries/SearchResults.vue
+++ src/components/countries/SearchResults.vue
@@ -1,10 +1,25 @@
 <template>
     <ul>
-      <li v-for="country in countries" :key="country.name.common">{{ country.name.common }}</li>
-      <li v-for="contry in countries" :key="country.nativeName.nvl">{{country.name.common }}</li>
+      <li class="texto" v-for="country in countries" :key="country.name.common">{{ country.name.common }}</li>
+      <li v-for="country in countries" :key="country.name.official">{{ country.name.official}}</li>
+      <!--<li v-for="country in countries" :key="country.nativeName.nvl">{{ country.nativeName.nvl }}</li>-->
     </ul>
   </template>
-  
+
+  <style>
+   .texto { 
+   width: 100%;
+}
+th, td {
+   width: 25%;
+   text-align: left;
+}
+
+
+
+
+  </style>
+
   <script>
   export default {
     name: 'SearchResults',
