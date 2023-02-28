ff4eeb5c48c6739aae92c588ee3bc0634526de5e
oszmondra <oszmodra@gmail.com>
2023-02-28 09:59:14 -0600

    arreglo de name

--- src/components/countries/SearchResults.vue
+++ src/components/countries/SearchResults.vue
@@ -1,6 +1,7 @@
 <template>
     <ul>
       <li v-for="country in countries" :key="country.name.common">{{ country.name.common }}</li>
+      <li v-for="contry in countries" :key="country.nativeName.nvl">{{country.name.common }}</li>
     </ul>
   </template>
   
