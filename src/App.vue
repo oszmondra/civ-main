374f8fc2171c0466a2756ca29ea5ca84e97d2412
EskinMH <eskinmontiel5@gmail.com>
2023-02-27 21:15:24 -0600

    add componente searchResult y modifique app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <Navbar brand-name="Countries API" />
   <router-view />
+  <Search @search-results="showResults"></Search>
+  <SearchResults v-if="searched" :countries="countries"></SearchResults>
 </template>
 
 <script>
 import Navbar from '../src/components/Navbar/index.vue';
+import Search from '../src/components/countries/Search.vue'
+import SearchResults from '../src/components/countries/SearchResults.vue'
 
-export default({
+export default {
   components: {
-    Navbar
+    Navbar,
+    Search,
+    SearchResults
+  },
+  data() {
+    return {
+      searched: false,
+      countries: []
+    }
+  },
+  methods: {
+    showResults(countries) {
+      this.searched = true
+      this.countries = countries
+    }
   }
-})
-
-
+}
 </script>
---
+++ src/components/countries/SearchResults.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+      <li v-for="country in countries" :key="country.name.common">{{ country.name.common }}</li>
+    </ul>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SearchResults',
+    props: {
+      countries: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+  </script>
+  
